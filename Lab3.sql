f7760785e32a00ece4597aa5d5abde1f7e087c3d
Jacob <jacob.levinson1@marist.edu>
2016-02-10 19:49:44 -0500

    Lab3
    Lab3

---
+++ Lab3.sql
@@ -0,0 +1,26 @@
+﻿SELECT ordno, dollars
+FROM orders;
+SELECT name, city
+FROM agents
+WHERE name = 'Smith';
+SELECT pid, name, priceusd
+FROM products
+WHERE quantity > 208000;
+SELECT name, city
+FROM customers
+WHERE city = 'Dallas';
+SELECT name
+FROM agents
+WHERE city != 'New York' and city != 'Tokyo';
+SELECT *
+FROM products
+WHERE city != 'Dallas' and city != 'Duluth' and priceusd >= 1;
+SELECT *
+FROM orders
+WHERE mon = 'jan' or mon = 'mar';
+SELECT *
+FROM orders
+WHERE mon = 'feb' and dollars < 500;
+SELECT *
+FROM orders
+WHERE cid = c005;
